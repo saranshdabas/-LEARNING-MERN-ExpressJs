37de4825ac14932ff7df33aec9ee878df959579f
Apple <sdjee2015@gmail.com>
2020-12-15 23:08:34 +0530

    Server creation and Rest API using expressJS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # -LEARNING-MERN-ExpressJs
+
+Run npm init to install express before running the main.js
--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 //Express JS is a light weight library which helps in network operations
 //like creating and handling of server, API requests etc.
+
+//expres() runs the index.js file of express module
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//Root directory API
+app.get("/", (req, res) => {
+  res.send("Hello world! Express is awesome...");
+});
+
+// /users API
+app.get("/users", (req, res) => {
+  res.send({ Names: "Saransh, Pranjal, Rahul" });
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
