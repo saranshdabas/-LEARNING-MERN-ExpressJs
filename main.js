387e3f7e9e1cc5f8897ebaa803db3933ba62263c
Apple <sdjee2015@gmail.com>
2020-12-16 23:28:49 +0530

    Static files in ExpresJS

--- main.js
+++ main.js
@@ -3,8 +3,11 @@
 
 //expres() runs the index.js file of express module
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 //Root directory API
 app.get("/", (req, res) => {
@@ -16,4 +19,14 @@ app.get("/users", (req, res) => {
   res.send({ Names: "Saransh, Pranjal, Rahul" });
 });
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+//---------------Static files-------------------
+//https://expressjs.com/en/starter/static-files.html
+
+//We need to specify the path of static file so that node js can include that and compile in html
+// /abc is the virtual path to secure our app so that no one knows the actual path in inspect etc.
+app.use("/abc", express.static(path.join(__dirname, "public")));
+
+// /meme API
+app.get("/meme", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
